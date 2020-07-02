d410de90aa4c554baf605db60eee942c5cdca672
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-01 21:14:36 -0500

    events controller, views - all basic functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,17 @@ Category.destroy_all
 EventCategory.destroy_all
 
 
- user1 = User.create(username: "guy1")
- user2 = User.create(username: "guy2")
- user3 = User.create(username: "guy3")
- user4 = User.create(username: "guy4")
+user1 = User.create(username: "guy1")
+user2 = User.create(username: "guy2")
+user3 = User.create(username: "guy3")
+user4 = User.create(username: "guy4")
 
 
- event1 = Event.create(title: "event1", user_id: user1.id)
- event2 = Event.create(title: "event2", user_id: user2.id)
+event1 = Event.create(title: "event1", user_id: user1.id)
+event2 = Event.create(title: "event2", user_id: user2.id)
 
 attendance1 = Attendance.create(user_id: user1.id, event_id: event2.id)
 attendance2 = Attendance.create(user_id: user2.id, event_id: event1.id)
 attendance3 = Attendance.create(user_id: user2.id, event_id: event2.id)
 attendance4 = Attendance.create(user_id: user4.id, event_id: event1.id)
 attendance5 = Attendance.create(user_id: user3.id, event_id: event1.id)
-
